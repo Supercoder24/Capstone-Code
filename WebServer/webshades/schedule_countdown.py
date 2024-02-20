985d69b8e445f91c5647383d9f3aa177266fc869
ThePracticingProgrammer <83358911+ThePracticingProgrammer@users.noreply.github.com>
2024-02-20 11:14:43 -0600

    Update schedule_countdown.py

--- WebServer/webshades/schedule_countdown.py
+++ WebServer/webshades/schedule_countdown.py
@@ -1,5 +1,6 @@
 import time
 from datetime import datetime, timedelta
+from webshades.db import get_db
 def start_countdown(input_list, time_of_execution, event_lapsed = False):
   t = time_of_execution
   i = input_list
@@ -20,3 +21,17 @@ def start_countdown(input_list, time_of_execution, event_lapsed = False):
   else:
     next_occurrence = time.time()+int(seconds_until.total_seconds())
     return next_occurrence
+
+while True:
+  min = db.execute('SELECT MIN(countdown) FROM schedule')
+  if min < time.time():
+    req = db.execute('SELECT days, vars, tod, ip FROM schedule INNER JOIN rooms ON rooms.id=shedule.room_id WHERE countdown = ?',(min)).fetchone()
+    db.execute('UPDATE schedule SET countdown =? WHERE countdown = ? and vars=?',(start_countdown(req[0],req[2],True),min,req[1])
+    db.execute('UPDATE rooms SET variables = ? WHERE ip = ?',(req[1],req[3])
+    db.commit()
+    with open(current_app.config['VARIABLES'] + req['ip'] + '.txt', 'w') as file:
+                                        file.write(new_variables)
+    continue
+  time.sleep(30)
+    
+    
