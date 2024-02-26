2dc4e8edc56d253ac7265b69cc3ab57cd1694c18
ThePracticingProgrammer <83358911+ThePracticingProgrammer@users.noreply.github.com>
2024-02-26 14:37:40 -0600

    Update schema.sql

--- WebServer/webshades/schema.sql
+++ WebServer/webshades/schema.sql
@@ -5,6 +5,7 @@ DROP TABLE IF EXISTS rooms;
 
 CREATE TABLE users (
   id INTEGER PRIMARY KEY AUTOINCREMENT,
+  admin BOOLEAN NOT NULL,
   username TEXT UNIQUE NOT NULL,
   password TEXT NOT NULL
 );
@@ -12,7 +13,6 @@ CREATE TABLE users (
 CREATE TABLE access (
   user_id INTEGER NOT NULL,
   room_id INTEGER NOT NULL,
-  admin BOOLEAN NOT NULL,
   last_accessed TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP -- TODO: Set timestamp to some other default value
 );
 
